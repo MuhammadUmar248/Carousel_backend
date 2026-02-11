73b81b8773953473195c2e2ff3c8026a7ca2377a
APPLE <apple@APPLEs-MacBook-Pro.local>
2026-02-12 03:06:45 +0500

    Add basic route

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from routes.createpost import router as createpost
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
 
 
 
@@ -25,4 +27,9 @@ templates = Jinja2Templates(directory="templates")
 
 
 app.include_router(createpost , prefix="/createpost")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def root():
+    return "<h1>Carousel Backend is Live ✅</h1>"
 
